a684ea2389d0572efefecbbb0da1975e19bf9fec
Kevin <kraus.kevin@protonmail.com>
2022-03-06 12:40:36 +0100

    background added footer added

--- src/components/Home/hero.tsx
+++ src/components/Home/hero.tsx
@@ -1,18 +1,23 @@
+import { GatsbyImage } from 'gatsby-plugin-image/dist/src/components/gatsby-image.browser';
 import React from 'react';
+import Background from '../background';
 
 export default function Hero() {
   return (
-    <main className="relative mx-auto mt-20 mx-10">
-      <div className="grid grid-flow-col gap-4">
-        <div className="flex justify-evenly">
-          <div className="h-20 bg-gray-100 flex items-center px-20">
-            <div>Full Stack Development</div>
-          </div>
-          <div className="h-20 bg-gray-400 flex items-center px-20">
-            <div>Graphic Design</div>
+    <>
+      <Background />
+      <main className="relative mx-auto mt-20 mx-10">
+        <div className="grid grid-flow-col gap-4 h-20">
+          <div className="flex justify-evenly">
+            <div className="h-20 bg-gray-100 flex items-center px-20">
+              <GatsbyImage></GatsbyImage>
+            </div>
+            <div className="h-20 bg-gray-400 flex items-center px-20">
+              <div>Graphic Design</div>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
